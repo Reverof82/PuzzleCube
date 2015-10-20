db4f20c5daece6b47bd13b7b3221866b4f3199fa
Reverof82 <thomasrichardlee@gmail.com>
2015-10-20 11:13:52 -0400

    Initial commit of the project after creating repository.

---
+++ Assets/Scripts/CubeGenerator.cs
@@ -0,0 +1,174 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CubeGenerator : MonoBehaviour
+{
+    public GameObject cubeletPrefab;
+    public GameObject rotatorPrefab;
+    public Material[] textureArray = new Material[6];
+
+    public int xSize;
+    public int ySize;
+    public int zSize;
+
+    Vector3 startingPosition;
+    Vector3 buildingPosition;
+
+    public List<GameObject> cubelets = new List<GameObject>();
+    public List<GameObject> rotators = new List<GameObject>();
+
+    int offset;
+
+    // Use this for initialization
+    void Start()
+    {
+        Init();
+        BuildCube();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    // Initialization variables
+    void Init()
+    {
+        offset = (int)cubeletPrefab.transform.localScale.x;
+        startingPosition = transform.position;
+        buildingPosition = new Vector3(xSize / 2.0f, ySize / 2.0f, zSize / 2.0f);
+    }
+
+    // Basic cube construction
+    void BuildCube()
+    {
+        // Move the cube into a good building position
+        this.transform.position = buildingPosition;
+        // Create the objects that we will use to facilitate rotation
+        CreateRotationMatrix();
+        for (int x = 0; x < xSize; x++)
+        {
+            for (int y = 0; y < ySize; y++)
+            {
+                for (int z = 0; z < zSize; z++)
+                {
+                    if (((x == 0 || x == xSize - 1) || (y == 0 || y == ySize - 1)) || (z == 0 || z == zSize - 1))
+                    {
+                        if ((x == 0 && y == 0 && z == 0) || (x == 0 && y == 0 && z == zSize - 1) || (x == 0 && y == ySize - 1 && z == 0) || (x == 0 && y == ySize - 1 && z == zSize - 1) || (x == xSize - 1 && y == 0 && z == 0) || (x == xSize - 1 && y == 0 && z == zSize - 1) || (x == xSize - 1 && y == ySize - 1 && z == 0) || (x == xSize - 1 && y == ySize - 1 && z == zSize - 1))
+                        {
+                            cubeletPrefab = (GameObject)Instantiate(cubeletPrefab, new Vector3((x + (offset / 2.0f) * offset), (y + (offset / 2.0f) * offset), (z + (offset / 2.0f) * offset)), Quaternion.identity);
+                            cubeletPrefab.transform.SetParent(transform);
+                            cubeletPrefab.name = "Corner";
+                            cubelets.Add(cubeletPrefab);
+                        }
+                        if ((x >= 1 && x <= xSize - 2 && y >= 1 && y <= ySize - 2) || (y >= 1 && y <= ySize - 2 && z >= 1 && z <= zSize - 2) || (x >= 1 && x <= xSize - 2 && z >= 1 && z <= zSize - 2))
+                        {
+                            cubeletPrefab = (GameObject)Instantiate(cubeletPrefab, new Vector3((x + (offset / 2.0f) * offset), (y + (offset / 2.0f) * offset), (z + (offset / 2.0f) * offset)), Quaternion.identity);
+                            cubeletPrefab.transform.SetParent(transform);
+                            cubeletPrefab.name = "Center";
+                            cubelets.Add(cubeletPrefab);
+                        }
+                        if (((x > 0 && x < xSize - 1) && (y == 0 || y == ySize - 1) && (z == 0 || z == zSize - 1)) || ((x == 0 || x == xSize - 1) && (y > 0 && y < ySize - 1) && (z == 0 || z == zSize - 1)) || ((x == 0 || x == xSize - 1) && (y == 0 || y == ySize - 1) && (z > 0 && z < zSize - 1)))
+                        {
+                            cubeletPrefab = (GameObject)Instantiate(cubeletPrefab, new Vector3((x + (offset / 2.0f) * offset), (y + (offset / 2.0f) * offset), (z + (offset / 2.0f) * offset)), Quaternion.identity);
+                            cubeletPrefab.transform.SetParent(transform);
+                            cubeletPrefab.name = "Edge";
+                            cubelets.Add(cubeletPrefab);
+                        }
+                    }
+                }
+            }
+        }
+        AssignFaceColor();
+        // Move the cube back to its original position
+        this.transform.position = startingPosition;
+    }
+
+    // Assign the colors or textures to each face of the cube
+    void AssignFaceColor()
+    {
+        foreach (GameObject cubelet in cubelets)
+        {
+            if ((cubelet.transform.position.x - offset / 2.0f) == 0)
+            {
+                cubelet.transform.GetChild(5).GetComponent<MeshRenderer>().material = textureArray[4];
+            }
+            if ((cubelet.transform.position.x - offset / 2.0f) == xSize - 1)
+            {
+                cubelet.transform.GetChild(6).GetComponent<MeshRenderer>().material = textureArray[5];
+            }
+            if ((cubelet.transform.position.y - offset / 2.0f) == 0)
+            {
+                cubelet.transform.GetChild(2).GetComponent<MeshRenderer>().material = textureArray[1];
+            }
+            if ((cubelet.transform.position.y - offset / 2.0f) == ySize - 1)
+            {
+                cubelet.transform.GetChild(1).GetComponent<MeshRenderer>().material = textureArray[0];
+            }
+            if ((cubelet.transform.position.z - offset / 2.0f) == 0)
+            {
+                cubelet.transform.GetChild(3).GetComponent<MeshRenderer>().material = textureArray[2];
+            }
+            if ((cubelet.transform.position.z - offset / 2.0f) == zSize - 1)
+            {
+                cubelet.transform.GetChild(4).GetComponent<MeshRenderer>().material = textureArray[3];
+            }
+        }
+    }
+
+    void CreateRotationMatrix()
+    {
+        GameObject rotParent = (GameObject)Instantiate(rotatorPrefab, new Vector3((xSize / 2.0f), (ySize / 2.0f), (zSize / 2.0f)), Quaternion.identity);
+        rotParent.name = "Rotation Core";
+        rotators.Add(rotParent);
+        rotParent.transform.SetParent(transform);
+
+        for (int i = 0; i < xSize; i++)
+        {
+            Vector3 temp = new Vector3((i + (offset / 2.0f) * offset), (ySize / 2.0f), (zSize / 2.0f));
+            if (temp != transform.position)
+            {
+                rotatorPrefab = (GameObject)Instantiate(rotatorPrefab, temp, Quaternion.identity);
+                //facePrefab.transform.localScale = new Vector3(offset, offset, offset);
+                rotatorPrefab.name = "XFace " + rotatorPrefab.transform.position;
+                rotatorPrefab.transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
+                rotators.Add(rotatorPrefab);
+                rotatorPrefab.transform.SetParent(rotParent.transform);
+            }
+        }
+        for (int i = 0; i < ySize; i++)
+        {
+            Vector3 temp = new Vector3((xSize / 2.0f), (i + (offset / 2.0f) * offset), (zSize / 2.0f));
+            if (temp != transform.position)
+            {
+                rotatorPrefab = (GameObject)Instantiate(rotatorPrefab, temp, Quaternion.identity);
+                //facePrefab.transform.localScale = new Vector3(offset, offset, offset);
+                rotatorPrefab.name = "YFace " + rotatorPrefab.transform.position;
+                rotatorPrefab.transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
+                rotators.Add(rotatorPrefab);
+                rotatorPrefab.transform.SetParent(rotParent.transform);
+            }
+        }
+        for (int i = 0; i < zSize; i++)
+        {
+            Vector3 temp = new Vector3((xSize / 2.0f), (ySize / 2.0f), (i + (offset / 2.0f) * offset));
+            if (temp != transform.position)
+            {
+                rotatorPrefab = (GameObject)Instantiate(rotatorPrefab, temp, Quaternion.identity);
+                //facePrefab.transform.localScale = new Vector3(offset, offset, offset);
+                rotatorPrefab.name = "ZFace " + rotatorPrefab.transform.position;
+                rotatorPrefab.transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
+                rotators.Add(rotatorPrefab);
+                rotatorPrefab.transform.SetParent(rotParent.transform);
+            }
+        }
+    }
+
+    public int Offset
+    {
+        get { return offset; }
+        set { offset = value; }
+    }
+}
---
+++ Assets/Scripts/RotationManager.cs
@@ -0,0 +1,268 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class RotationManager : MonoBehaviour
+{
+    enum Swipe { None, Up, Down, Left, Right };
+    enum Face { None, Up, Down, Left, Right, Front, Back };
+    Face selectedFace;
+    Swipe swipeDirection;
+    GameObject cube;
+    Transform cameraTarget;
+    GameObject cubelet;
+    CubeGenerator gc;
+    List<GameObject> groupToRotate = new List<GameObject>();
+    bool animating;
+    bool dragging = false;
+    bool mousePressed;
+    Vector2 mouseStartPosition;
+    Vector2 mouseEndPosition;
+    Vector2 currentSwipe;
+    float cubeWidth = 0.0f;
+    float distance = 5.0f;
+
+    float xSpeed = 180.0f;
+    float ySpeed = 90.0f;
+
+    float yMinLimit = -80.0f;
+    float yMaxLimit = 80.0f;
+
+    float x = 0.0f;
+    float y = 0.0f;
+
+    // Use this for initialization
+    void Start()
+    {
+        cube = GameObject.Find("CubeCore");
+        cameraTarget = cube.transform;
+        gc = cube.GetComponent<CubeGenerator>();
+        cubeWidth = gc.xSize * gc.Offset;
+        distance = cubeWidth + 3.0f;
+
+        Vector3 angles = transform.eulerAngles;
+        x = angles.y;
+        y = angles.x;
+        PositionCamera();
+    }
+
+    void LateUpdate()
+    {
+        if (!Animating && Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                cubelet = hit.transform.gameObject;
+                selectedFace = GethitFace(hit);
+                mousePressed = true;
+                mouseStartPosition = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+            }
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            if (mousePressed)
+            {
+                mouseEndPosition = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+                currentSwipe = new Vector2(mouseEndPosition.x - mouseStartPosition.x, mouseEndPosition.y - mouseStartPosition.y);
+                currentSwipe.Normalize();
+                if (currentSwipe.y > 0 && currentSwipe.x > -0.5f && currentSwipe.x < 0.5f)
+                {
+                    swipeDirection = Swipe.Up;
+                }
+                if (currentSwipe.y < 0 && currentSwipe.x > -0.5f && currentSwipe.x < 0.5f)
+                {
+                    swipeDirection = Swipe.Down;
+                }
+                if (currentSwipe.x > 0 && currentSwipe.y > -0.5f && currentSwipe.y < 0.5f)
+                {
+                    swipeDirection = Swipe.Right;
+                }
+                if (currentSwipe.x < 0 && currentSwipe.y > -0.5f && currentSwipe.y < 0.5f)
+                {
+                    swipeDirection = Swipe.Left;
+                }
+                if (swipeDirection != Swipe.None)
+                {
+                    print("Swiped " + swipeDirection + " on the " + selectedFace + " face.");
+                }
+                print("Cube location: " + cubelet.transform.position.x + ", " + cubelet.transform.position.y + ", " + cubelet.transform.position.z);
+                AssignCubeletsToRotator(cubelet, swipeDirection, selectedFace);
+                mousePressed = false;
+                cubelet = null;
+                swipeDirection = Swipe.None;
+            }
+        }
+
+        if (animating)
+        {
+            transform.LookAt(cameraTarget);
+            x += xSpeed * 0.005f;
+            PositionCamera();
+        }
+
+        if (cameraTarget && Input.GetMouseButton(0) && cubelet == null)
+        {
+            dragging = true;
+            x += Input.GetAxis("Mouse X") * xSpeed * 0.05f;
+            y -= Input.GetAxis("Mouse Y") * ySpeed * 0.05f;
+
+            y = ClampAngle(y, yMinLimit, yMaxLimit);
+
+            PositionCamera();
+        }
+        else
+        {
+            dragging = false;
+        }
+    }
+
+    void PositionCamera()
+    {
+        Quaternion rotation = Quaternion.Euler(y, x, 0);
+        Vector3 position = rotation * new Vector3(0.0f, 0.0f, -distance) + cameraTarget.position;
+        transform.rotation = rotation;
+        transform.position = position;
+    }
+
+    float ClampAngle(float angle, float min, float max)
+    {
+        if (angle < -360)
+        {
+            angle += 360;
+        }
+        if (angle > 360)
+        {
+            angle -= 360;
+        }
+        return Mathf.Clamp(angle, min, max);
+    }
+
+    Face GethitFace(RaycastHit hit)
+    {
+        Vector3 incomingVec = hit.normal - Vector3.up;
+        if (incomingVec == new Vector3(0, -1, -1))
+        {
+            return Face.Front;
+        }
+        if (incomingVec == new Vector3(0, -1, 1))
+        {
+            return Face.Back;
+        }
+        if (incomingVec == new Vector3(0, 0, 0))
+        {
+            return Face.Up;
+        }
+        if (incomingVec == new Vector3(0, -2, 0))
+        {
+            return Face.Down;
+        }
+        if (incomingVec == new Vector3(-1, -1, 0))
+        {
+            return Face.Left;
+        }
+        if (incomingVec == new Vector3(1, -1, 0))
+        {
+            return Face.Right;
+        }
+        return Face.None;
+    }
+
+    void AssignCubeletsToRotator(GameObject cubeletClicked, Swipe swipeDirection, Face faceSelected)
+    {
+        Vector3 axis = Vector3.up;
+        float angle = 90.0f;
+        groupToRotate.Clear();
+        
+        if (swipeDirection == Swipe.Up || swipeDirection == Swipe.Down)
+        {
+            if (swipeDirection == Swipe.Down)
+            {
+                angle = -angle;
+            }
+            if (faceSelected == Face.Back || faceSelected == Face.Left || faceSelected == Face.Up)
+            {
+                angle = -angle;
+            }
+            foreach (GameObject cubelet in gc.cubelets)
+            {
+                if (faceSelected == Face.Front || faceSelected == Face.Back)
+                {
+                    axis = Vector3.right;
+                    if (cubelet.transform.position.x == cubeletClicked.transform.position.x)
+                    {
+                        groupToRotate.Add(cubelet);
+                    }
+                }
+                if(faceSelected == Face.Left || faceSelected == Face.Right)
+                {
+                    axis = Vector3.forward;
+                    if (cubelet.transform.position.z == cubeletClicked.transform.position.z)
+                    {
+                        groupToRotate.Add(cubelet);
+                    }
+                }
+                if (faceSelected == Face.Up || faceSelected == Face.Down)
+                {
+                    axis = Vector3.up;
+                    if (cubelet.transform.position.y == cubeletClicked.transform.position.y)
+                    {
+                        groupToRotate.Add(cubelet);
+                    }
+                }
+            }
+        }
+        if (swipeDirection == Swipe.Left || swipeDirection == Swipe.Right)
+        {
+            if (swipeDirection == Swipe.Right)
+            {
+                angle = -angle;
+            }
+            foreach (GameObject cubelet in gc.cubelets)
+            {
+                if (faceSelected == Face.Front || faceSelected == Face.Back)
+                {
+                    if (cubelet.transform.position.y == cubeletClicked.transform.position.y)
+                    {
+                        groupToRotate.Add(cubelet);
+                    }
+                }
+                if (faceSelected == Face.Left || faceSelected == Face.Right)
+                {
+                    if (cubelet.transform.position.y == cubeletClicked.transform.position.y)
+                    {
+                        groupToRotate.Add(cubelet);
+                    }
+                }
+                if (faceSelected == Face.Up || faceSelected == Face.Down)
+                {
+                    if (cubelet.transform.position.x == cubeletClicked.transform.position.x)
+                    {
+                        groupToRotate.Add(cubelet);
+                    }
+                }
+            }
+        }
+        print(groupToRotate.Count);
+        foreach (GameObject test in groupToRotate)
+        {
+            test.transform.RotateAround(cube.transform.position, axis, angle);
+            test.transform.position = new Vector3(Mathf.Round(test.transform.position.x), Mathf.Round(test.transform.position.y), Mathf.Round(test.transform.position.z));
+        }
+    }
+
+    public bool Animating
+    {
+        get { return animating; }
+        set { animating = value; }
+    }
+
+    public bool Dragging
+    {
+        get { return dragging; }
+        set { dragging = value; }
+    }
+}
